455baf805603f1a667c2fd24886e255d294faae1
Michal Z <zajda.michal@gmail.com>
2010-12-01 19:10:28 +0100

    link m/r WiP

--- src/demo.erl
+++ src/demo.erl
@@ -7,11 +7,11 @@
 %% API
 -export([populate/0, relations1/0, relations2/0, query_mr/0, query_mr_link/0]).
 
+-export([sum/2, get_balance/3, calc_balance/3]).
 %% Map & Reduce
--export([sum/2, get_balance/3]).
 
 
--record(account, { intrests = undefined :: integer(),
+-record(account, { intrests = undefined :: float(),
                    balance = 0 :: integer()
                  }).
 -record(details, { description :: string(),
@@ -24,22 +24,22 @@
 
 populate() ->
     save(<<"account_balance">>, <<"Michal">>, 
-         #account{intrests = 3,
+         #account{intrests = 0.03,
                   balance = 123}),
     save(<<"account_balance">>, <<"Person1">>, 
-         #account{intrests = 2,
+         #account{intrests = 0.02,
                   balance = 500}),
     save(<<"account_balance">>, <<"Bob">>, 
-         #account{intrests = 2,
+         #account{intrests = 0.02,
                   balance = 45}),
     save(<<"account_balance">>, <<"Alice">>, 
-         #account{intrests = 9,
+         #account{intrests = 0.09,
                   balance = 600}),
     save(<<"account_details">>, <<"Alice">>, 
          #details{description = "My account",
                   creation_date = calendar:local_time(),
                   address="Wonderland 56"}),
-    save(<<"account_secret">>, <<"Alice">>, 
+    save(<<"account_savings">>, <<"Alice">>, 
          #account{balance = 10000}).
 
 relations1() ->
@@ -47,10 +47,10 @@ relations1() ->
     {ok, Obj} = Client:get(<<"account_details">>, <<"Alice">>,1),
     Meta = riak_object:get_metadata(Obj),
     Links = dict:fetch(<<"Links">>, Meta),
-    {value, {{SecretBucket, SecretKey},_}} = lists:keysearch(<<"secret">>, 2, Links),
+    {value, {{SecretBucket, SecretKey},_}} = lists:keysearch(<<"savings">>, 2, Links),
     {ok, Linked} = Client:get(SecretBucket, SecretKey, 1),
     io:format("~p~n linked to~n ~p~n",[riak_object:get_value(Obj), 
-                                   riak_object:get_value(Linked)]).
+                                       riak_object:get_value(Linked)]).
 
 relations2() ->
     {ok, C} = riak:local_client(),
@@ -58,7 +58,7 @@ relations2() ->
                          C:get(Bucket, <<"Alice">>, 1) || 
                             Bucket <- [ <<"account_details">>, 
                                         <<"account_balance">>,
-                                        <<"account_secret">>
+                                        <<"account_savings">>
                                       ]]),
     io:format("~p~n",[Related]).
 
@@ -72,14 +72,32 @@ query_mr() ->
     io:format("~p~n",[Result]).
 
 query_mr_link() ->
-    ok.
+    {ok, Client} = riak:local_client(),
+    Result = 
+        Client:mapred_bucket(<<"account_details">>,
+                             [{link, '_', '_', false},
+                              {map, {modfun, ?MODULE, calc_balance}, none, false},
+                              {reduce, {modfun, ?MODULE, sum}, none, true}]),
+    
+    io:format("~p~n",[Result]).
+
+calc_balance(Doc, _KeyData, _Arg) ->
+    Data = riak_object:get_value(Doc),
+    Intrests = Data#account.balance*(Data#account.intrests+1),
+    io:format("> ~p~n",[Intrests]),
+    [Intrests].
 
 get_balance(Doc, _KeyData, _Arg) ->
     Data = riak_object:get_value(Doc),
     [Data#account.balance].
 
 sum(ValueList, _Arg) ->
-    [lists:sum(lists:flatten(ValueList))].
+    Fun = fun([X],Acc0) when is_integer(X) ->
+                  Acc0+X;
+             (_,Acc0) ->
+                  Acc0
+          end,
+    [lists:foldl(Fun, 0, ValueList)].
     
 
             
@@ -91,7 +109,8 @@ sum(ValueList, _Arg) ->
 save(Bucket = <<"account_details">>, Key = <<"Alice">>, Data) ->
     Obj = riak_object:new(Bucket, Key, Data),
     Meta = dict:store(<<"Links">>,
-                      [{{<<"account_secret">>, <<"Alice">>}, <<"secret">>}],
+                      [{{<<"account_savings">>, <<"Alice">>}, <<"savings">>},
+                       {{<<"account_balance">>, <<"Alice">>}, <<"regular">>}],
                       riak_object:get_metadata(Obj)),
     ObjectWithMeta = riak_object:update_metadata(Obj, Meta),
     {ok, C} = riak:local_client(),
